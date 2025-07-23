89bb1bd264e004356bebe7a2ae0ed9228d2af79f
Joseph Sabo <jsephsabo@gmail.com>
2025-07-22 23:16:28 -0600

    Implement user CRUD and Firebase integration

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
  */
 
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: 'local.env' });
 
 /**
  * SETUP THE EXPRESS SERVER
@@ -16,11 +16,23 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+import admin from './firebase.js';
+import fs from 'fs';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const serviceAccount = JSON.parse(fs.readFileSync(path.join(__dirname, 'soma-tracker-firebase-adminsdk-fbsvc-b0d7b7669b.json')));
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 // Templating
 // app.set('view engine', 'ejs');
--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 import debugController from './controller/debug_controller.js';
+import usersController from './controller/users_controller.js';
 
 router.use('/debug', debugController);
+router.use('/users', usersController);
 
 export default router;
