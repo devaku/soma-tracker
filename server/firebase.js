8051852b330ca36317fc689a9c9947d83003810d
Alejo Kim Uy <alejokimuy@gmail.com>
2025-07-23 08:37:06 -0600

    Formatted certificate

--- server/firebase.js
+++ server/firebase.js
@@ -1,20 +1,13 @@
 import admin from 'firebase-admin';
 import fs from 'fs';
-import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const serviceAccount = JSON.parse(
-	fs.readFileSync(
-		path.join(
-			__dirname,
-			'soma-tracker-firebase-adminsdk-fbsvc-b0d7b7669b.json'
-		)
-	)
-);
+const filepath = `${__dirname}/certs/soma-tracker-firebase-adminsdk-fbsvc-b0d7b7669b.json`;
+const serviceAccount = JSON.parse(fs.readFileSync(filepath));
 
 if (!admin.apps.length) {
 	admin.initializeApp({
