c3c0681fda36003eaa6458a585bc2d6cc6b3c7f3
Bdopz <bdopz0883@gmail.com>
2025-08-13 16:15:43 -0600

    Login/Register styling

--- app/page.js
+++ app/page.js
@@ -34,19 +34,44 @@ export default function LandingPage() {
 	}
 
 	return (
-		<main>
-			<div>
-				<label htmlFor="email">Email: </label>
-				<input type="email" name="email" id="" />
+		<main className="flex flex-col mx-auto items-center mt-20 border border-gray-300 bg-white w-fit p-10">
+			<h1 className="font-bold text-4xl text-left w-full">Welcome</h1>
+			<h2 className="text-gray-500 text-left w-full mb-8">
+				Login or Register
+			</h2>
+			<div className="border border-gray-300 w-[400px] mb-5">
+				<input
+					className="p-2 w-full"
+					type="email"
+					name="email"
+					id=""
+					placeholder="Enter address"
+				/>
 			</div>
-			<div>
-				<label htmlFor="password">Password</label>
-				<input type="password" name="password" id="" />
+			<div className="border border-gray-300 w-[400px] mb-5">
+				<input
+					className="p-2 w-full"
+					type="password"
+					name="password"
+					id=""
+					placeholder="Password"
+				/>
 			</div>
-			<div>
-				<button onClick={handleLogin}>Login</button>
-				<br />
-				<button onClick={handleRegister}>Register</button>
+			<div className="w-[70%] bg-blue-500 py-2 mb-3 text-white rounded-md hover:bg-blue-600 hover:cursor-pointer">
+				<button
+					className="w-full hover:cursor-pointer"
+					onClick={handleLogin}
+				>
+					Login
+				</button>
+			</div>
+			<div className="w-[70%] border border-gray-300 py-2 mb-3 rounded-md hover:bg-gray-100 hover:cursor-pointer">
+				<button
+					className="w-full hover:cursor-pointer"
+					onClick={handleRegister}
+				>
+					Register
+				</button>
 			</div>
 		</main>
 	);
